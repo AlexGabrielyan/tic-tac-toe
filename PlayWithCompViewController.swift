13f3c0caa937f66438545658a52057966ebbe358
Aleksandr Gabrielyan <aleksandrgabrielyn@Aleksandrs-MacBook-Pro.local>
2022-05-02 12:56:01 +0400

    init commit

---
+++ PlayWithCompViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PlayWithCompViewController.swift
+//  X0Fun
+//
+//  Created by Aleksandr Gabrielyan on 19.04.22.
+//
+
+import UIKit
+
+class PlayWithCompViewController: UIViewController {
+    @IBAction func compButton(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ X0Fun/StartViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  X0Fun
+//
+//  Created by Vahan Grigoryan on 4/19/22.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
